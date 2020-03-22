6ef71bdb68c28bf7b6ecb415696219ccd587a359
Jefferson Estevam <jefferson.e.barbosa@accenture.com>
2020-03-22 19:30:15 -0300

    Add heroku verification

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Server = require('./src/server');
 const Envloader = require('./src/utils/envLoader');
 
-const loadEnvironments = new Envloader(`${__dirname}/`);
+if(!process.env.HEROKU_SERVER){
+    const loadEnvironments = new Envloader(`${__dirname}/`);
+}
 
 const server = new Server(process.env.SERVER_PORT);
 
